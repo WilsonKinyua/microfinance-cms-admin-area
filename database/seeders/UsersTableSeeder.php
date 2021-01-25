d8f8a86815c1311fad3d0727bbaaf250a25af161
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-01-25 16:28:16 +0300

    seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -13,8 +13,8 @@ class UsersTableSeeder extends Seeder
             [
                 'id'             => 1,
                 'name'           => 'Admin',
-                'email'          => 'admin@admin.com',
-                'password'       => bcrypt('password'),
+                'email'          => 'admin@microfinance.com',
+                'password'       => bcrypt('admin@microfinance.com'),
                 'remember_token' => null,
             ],
         ];
